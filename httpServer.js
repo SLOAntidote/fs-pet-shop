9cab0972628665698e4ad8768f7ec03df70e220a
SLOAntidote <valenzuela.lance@gmail.com>
2022-12-01 13:36:32 -0800

    Got error to work

--- httpServer.js
+++ httpServer.js
@@ -15,15 +15,21 @@ const server = http.createServer((req, res) => {
         const matches = petRegExp.exec(req.url);
         const petIndex = matches[1];
        
+        //attempt to throw error message if pet index does not exist
         readFile("./pets.json", "utf-8").then((text) => {
             const pets = JSON.parse(text);
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(pets));
-            console.log(pets[petIndex]);
+            if(petIndex <= pets.length && petIndex >= 0){
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(pets));
+                console.log(pets[petIndex]);
+            } else {
+                res.statusCode = 404;
+                res.setHeader("Content-Type", "text/plain");
+                res.end("Not Found");
+            }
         });
-    } else {
-        res.end('Unknown Request');
-    }
+    } 
+   
 });
 
 server.listen(7000, () => {
